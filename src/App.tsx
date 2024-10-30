a605ee0e8e3bd3a5c3d702056c5f114e97963285
lamrij <la227479@student.helha.be>
2024-10-30 15:43:08 +0100

    premier draft de la page principale, choix de films

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import React from 'react';
+import MovieCard from './components/MovieCard';
+
 function App() {
-  
   return (
-    <>
-    </>
-  )
+    <div style={{ 
+      minHeight: '100vh', 
+      display: 'flex', 
+      alignItems: 'center', 
+      background: '#f5f5f5' 
+      }}>
+      <MovieCard
+        title="APOCALYPSE NOW"
+        year="1979"
+        genre="WAR/ACTION"
+        posterUrl="./public/apocalypse-now.webp"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
